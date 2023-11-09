cce208cf89aff23d935ab167b8bad10c8e215a0f
“vishal-sharma-r” <sharmaji20rattan@gmail.com>
2023-11-09 16:13:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
