6fcb87fb6bc77d3916f44f431f4a87107f8d7d12
vishal.sharma <sharma.vishal.ext@siemens.com>
2024-07-29 17:07:33 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+// APP MAIN PAGE
 function App() {
   return (
     <div>
